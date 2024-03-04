24cde0f15eca024658d420c3b9aa1f6de0eea355
Aleksandr Gorbachev <menus12@mail.ru>
2024-03-04 23:22:10 +0100

    added upwork data processing

--- process.py
+++ process.py
@@ -1,37 +1,11 @@
 #!/usr/bin/env python3
 
+# common
 import os
 import json
 import re
 import argparse
 import datetime
-
-# Adding command line parameters
-parser = argparse.ArgumentParser(description='Upwork data processor')    
-parser.add_argument('--file', type=str, help='Projects dump from database')
-parser.add_argument('--update', type=bool, help='Update metadata (default is False)')
-args = parser.parse_args()
-
-if args.update == None:
-    args.update = False
-
-if args.file == None:
-    print (parser.print_help())
-    exit(1)
-
-start_time = datetime.datetime.now()
-
-f = open(args.file)                 # Opening JSON file
-source_file = json.loads(f.read())  # returns JSON object as  a dictionary
-f.close()                           # Closing file
-
-f = open("dumps/example_cv.json")   # Opening JSON file
-cv = json.loads(f.read())           # returns JSON object as  a dictionary
-f.close()                           # Closing file
-
-
-print ('File is ' + args.file)
-print ('---')
 
 #spacy
 import spacy
@@ -66,10 +40,18 @@ nltk.download(['stopwords','wordnet'])
 import warnings 
 warnings.filterwarnings('ignore')
 
-print ('--- Loading data')
-df = pd.read_csv("kaggle/Resume/Resume.csv")
-df = df.reindex(np.random.permutation(df.index))
-data = df.copy().iloc[0:20,]
+# Adding command line parameters
+parser = argparse.ArgumentParser(description='Upwork data processor')    
+parser.add_argument('--file', type=str, help='Projects dump from database')
+parser.add_argument('--update', type=bool, help='Update metadata (default is False)')
+args = parser.parse_args()
+
+if args.update == None:
+    args.update = False
+
+if args.file == None:
+    print (parser.print_help())
+    exit(1)
 
 nlp = spacy.load("en_core_web_lg")
 skill_pattern_path = "kaggle/jz_skill_patterns.jsonl"
@@ -89,6 +71,105 @@ def get_skills(text):
 
 def unique_skills(x):
     return list(set(x))
+
+def clear_text(text):
+    review = re.sub(
+        '(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?"',
+        " ",
+        text,
+    )
+    review = review.lower()
+    review = review.split()
+    lm = WordNetLemmatizer()
+    review = [
+        lm.lemmatize(word)
+        for word in review
+        if not word in set(stopwords.words("english"))
+    ]
+    review = " ".join(review)
+    return review
+
+start_time = datetime.datetime.now()
+
+print ('--- Loading data')
+
+f = open(args.file)                 # Opening JSON file
+source_file = json.loads(f.read())  # returns JSON object as  a dictionary
+f.close()                           # Closing file
+
+f = open("dumps/example_cv.json")   # Opening JSON file
+cv = json.loads(f.read())           # returns JSON object as  a dictionary
+f.close()                           # Closing file
+
+
+print ('File is ' + args.file)
+print ('---')
+
+total_skills = []
+
+for project in source_file:
+    # separate rate
+    rate_re = '\$\d+-\$\d+'
+    if "Rate not defined" in project['description']:
+        project['rate_min']  = None
+        project['rate_max']  = None
+        re.sub('Rate not defined', " ", project['description'])
+    rate = re.findall(rate_re, project['description'])
+    if len(rate) > 0:
+        project['rate_min']  = re.sub('\$','', rate[0].split('-')[0])
+        project['rate_max']  = re.sub('\$','', rate[0].split('-')[1])
+        project['description'] = re.sub(rate_re, " ", project['description'])
+
+    # separate skills to technologies
+    skills_re = 'Skills: ([a-zA-Z\d -\/;]+)'
+    if "Skills:" in project['description']:        
+        skills = re.findall(skills_re, project['description'])  
+        project['technologies'] = skills.pop()
+    else: project['technologies'] = "None"
+    project['description'] = re.sub(skills_re, " ", project['description'])
+
+    # separate category
+    cat_re = 'Category: ([a-zA-Z\d -\/;]+)'
+    if "Category:" in project['description']:        
+        category = re.findall(cat_re, project['description'])  
+        project['category'] = category.pop()
+    else: project['category'] = "None"
+    project['description'] = re.sub(cat_re, " ", project['description'])
+
+    # cleaning description
+    #project['clean_description'] = clear_text(project['description'])
+    project['clean_description'] = project['description']
+    
+    # checking for other skills
+    project['skills'] = get_skills(project["clean_description"].lower() + ' ' + project["technologies"].lower())
+    project['skills'] = unique_skills(project['skills'])
+    for skill in project['skills']:
+        total_skills.append(skill)
+
+# Plotting Project Categories
+fig = px.histogram(
+    source_file, x="category", title="Distribution of Project Categories"
+).update_xaxes(categoryorder="total descending")
+fig.show()
+
+# Plotting Project Countries
+fig = px.histogram(
+    source_file, x="country", title="Distribution of Countries"
+).update_xaxes(categoryorder="total descending")
+fig.show()
+
+# Plotting Project skills
+fig = px.histogram(
+    x=total_skills,
+    labels={"x": "Skills"},
+    title="Distribution of Skills",
+).update_xaxes(categoryorder="total descending")
+fig.show()
+
+df = pd.read_csv("kaggle/Resume/Resume.csv")
+df = df.reindex(np.random.permutation(df.index))
+data = df.copy().iloc[0:20,]
+
 print ('--- Cleaning data')
 clean = []
 for i in range(data.shape[0]):
@@ -114,10 +195,10 @@ print ('--- Extracting skills')
 data["skills"] = data["Clean_Resume"].str.lower().apply(get_skills)
 data["skills"] = data["skills"].apply(unique_skills)
 
-fig = px.histogram(
-    data, x="Category", title="Distribution of Jobs Categories"
-).update_xaxes(categoryorder="total descending")
-fig.show()
+# fig = px.histogram(
+#     data, x="Category", title="Distribution of Jobs Categories"
+# ).update_xaxes(categoryorder="total descending")
+# fig.show()
 
 Job_Cat = data["Category"].unique()
 Job_Cat = np.append(Job_Cat, "ALL")
